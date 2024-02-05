910eddfcc6102329ef044f5202ab536ecdeb2b66
sidou6x9 <119750201+sidou6x9@users.noreply.github.com>
2024-02-05 15:04:12 +0100

    y

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-
-
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElementsLeft = document.querySelectorAll(".hidden-left");
+hiddenElementsLeft.forEach((el) => observer.observe(el));
+const hiddenElementsRight = document.querySelectorAll(".hidden-right");
+hiddenElementsRight.forEach((el) => observer.observe(el));
+const hiddenElementsDown = document.querySelectorAll(".hidden-down");
+hiddenElementsDown.forEach((el) => observer.observe(el));
+const hiddenElementsUp = document.querySelectorAll(".hidden-up");
+hiddenElementsUp.forEach((el) => observer.observe(el));
